f987cb6264e4901620f9e2ee2d6fd0fea2a2fc8b
AXF <aidenfer@buffalo.edu>
2024-03-02 16:19:17 -0500

    Added quick access menu

--- home.js
+++ home.js
@@ -1,8 +1,14 @@
 document.addEventListener('DOMContentLoaded', function () {
     const menuToggle = document.getElementById('menuToggle');
+    const quickMenu = document.getElementById('quickMenu');
     
     menuToggle.addEventListener('click', function () {
       this.classList.toggle('active');
+      if (quickMenu.style.display === 'block'){
+        quickMenu.style.display = 'none';
+      } else {
+        quickMenu.style.display = 'block';
+      }
     });
   });
   
