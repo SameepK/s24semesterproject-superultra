6243bf49b7d497396e59a254a2f2543db2252a60
SameepK <112959293+SameepK@users.noreply.github.com>
2024-03-02 16:54:21 -0500

    Add files via upload

--- account_info.js
+++ account_info.js
@@ -1,25 +1,23 @@
 document.addEventListener("DOMContentLoaded", function() {
-            // Fetch account details
             fetchAccountDetails();
-
-            // Add event listener to the back button
             let btnBack = document.querySelector('button')
                 btnBack.addEventListener('click', () => {
                     window.history.back();
                 });
         });
-        function fetchAccountDetails() {
-            // Simulate data
-            const data = {
-                username: "JohnDoe",
-                email: "john.doe@example.com",
-                name: "John Doe",
-                "account number": "1234567890"
-            };
-
-            // Populate account details
-            document.getElementById('username').innerText = data.username;
-            document.getElementById('email').innerText = data.email;
-            document.getElementById('name').innerText = data.name;
-            document.getElementById('number').innerText = data['account number'];
+    function fetchAccountDetails() {
+            fetch('fetch_data.php')
+                .then(response => {
+                    if (!response.ok) {
+                        throw new Error('Network response was not ok');
+                    }
+                    return response.json();
+                })
+                .then(data => {
+                    document.getElementById('username').innerText = data.username;
+                    document.getElementById('email').innerText = data.email;
+                    document.getElementById('name').innerText = data.name;
+                    document.getElementById('number').innerText = data['account number'];
+                })
+                .catch(error => console.error('Error fetching user details:', error));
         }
