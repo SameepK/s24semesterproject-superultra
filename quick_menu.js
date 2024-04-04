de045eaa574f5367d9e74ea7d74483b74e5a8cda
Cejeyz <ddparris16@gmail.com>
2024-04-04 13:33:29 -0400

    Fixing the banks

---
+++ quick_menu.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const menuToggle = document.getElementById('menuToggle');
+    const quickMenu = document.getElementById('quickMenu');
+  
+    menuToggle.addEventListener('click', function () {
+  
+      if (quickMenu.style.display === 'block') {
+        quickMenu.style.display = 'none';
+      } else {
+        quickMenu.style.display = 'block';
+      }
+    });
+  });
+
